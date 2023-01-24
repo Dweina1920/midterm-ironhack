05c722c96e86c2156ac48d02ade7cd1960f321fc
Dina <dina.yamanid@gmail.com>
2023-01-24 18:03:09 +0100

    our services

---
+++ resources/js/slider.js
@@ -0,0 +1,13 @@
+<img src="./resources/images/testimonial-section/orbit.png" alt="" class="bg-review">
+// cda img en absolute + su top,right,left,bottom correcto
+<img />
+<img />
+<img />
+<img />
+
+<div>
+// flex height column + justify-between
+<h1>text 1</h1>
+<h1>text 2</h1>
+<h1>text 3</h1>
+</div>
