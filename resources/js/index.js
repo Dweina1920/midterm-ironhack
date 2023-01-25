d3beabf1260d9f41877e36aa62b81acb00cdbe8b
Dina <dina.yamanid@gmail.com>
2023-01-25 12:50:59 +0100

    home responsive

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,15 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+//hamburguer menu
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.nav-links');
+
+hamburguer.addEventListener('click', () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".links").forEach(n => n.addEventListener("click", () => {
+    hamburguer.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
